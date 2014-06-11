9b92407a86d260dc7583d2336795b75bcab18e37
Mimicar <gps@gpsportal.sk>
2014-06-11 14:44:24 +0200

    Update sk.js

--- lang/sk.js
+++ lang/sk.js
@@ -20,17 +20,17 @@
 			'Sent coordinates'									: 'Súradnice',
 			'Points'											: 'súradnice',
 			'Address'											: 'adresa',
-			'Drag to change<br />Double click to send'			: 'Drag to relocate.<br />Double-click to send.',
-			'There are no message to export'					: 'No messages to export.',
-			'Message is not delivered'							: 'Not delivered.',
-			'Session has been lost'								: 'Session lost.',
-			'XML comments'										: 'Attributes msgset: *name: unit or driver name; *type: type (unit, driver); *phones: available phone numbers. Attributes msg: *ts: message time (UTC); *type: message type (out - outgoing, in - incoming); *link: communication type (tcp ot sms); *flag: flags (1 - text, 2 - image, 3 - coordinates, 4 - alert); *text - message body.'
+			'Drag to change<br />Double click to send'			: 'Ťahaním presunúť.<br />Dvojklikom odoslať.',
+			'There are no message to export'					: 'Žiadne správy pre export.',
+			'Message is not delivered'							: 'Nedoručené.',
+			'Session has been lost'								: 'Relácia stratená.',
+			'XML comments'										: 'Atribúty msgset: *name: Názov vozidla alebo vodiča; *type: typ (vozidlo, vodič); *phones: dostupné telefónne čísla. Atribúty msg: *ts: čas správy (UTC); *type: typ správy (out - odoslané, in - doručené); *link: typ komunikácie (tcp alebo sms); *flag: vlajky (1 - text, 2 - obrázok, 3 - súradnice, 4 - poplach); *text - tvar správy.'
 		},
 		localization: {
-			'days'												: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-			'days_short'										: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-			'months'											: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-			'months_short'										: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+			'days'												: ['Nedeľa', 'Pondelok', 'Utorok', 'Streda', 'Štvrtok', 'Piatok', 'Sobota'],
+			'days_short'										: ['Ne', 'Po', 'Ut', 'St', 'Št', 'Pi', 'So'],
+			'months'											: ['Január', 'Február', 'Marec', 'Apríl', 'Máj', 'Jún', 'Júl', 'August', 'September', 'Október', 'November', 'December'],
+			'months_short'										: ['Jan', 'Feb', 'Mar', 'Apr', 'Maj', 'Jún', 'Júl', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec']
 		}
 	});
 }(jQuery, _);
