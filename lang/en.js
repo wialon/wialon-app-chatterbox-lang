9ee7ae1517741bb7b2ae97df42306fffaadfe053
Vitaly Makarevich <vima@gurtam.com>
2014-06-12 09:40:07 +0300

    Beautify localization property

--- lang/en.js
+++ lang/en.js
@@ -69,10 +69,10 @@
 			"mph": ""
 		},
 		localization: {
-			"days"												: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-			"days_short"										: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-			"months"											: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-			"months_short"										: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+			"days": ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+			"days_short": ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+			"months": ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+			"months_short": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 		}
 	});
 }(jQuery, _);
